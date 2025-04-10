5a40da342765c0d08684533543c17e95f482eac5
yassine <yassinechmarekh88@gmail.com>
2025-04-10 04:01:53 +0100

    add login method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { User } from './schemas/user.schema';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('api/auth')
 export class AuthController {
@@ -15,5 +16,10 @@ export class AuthController {
     @Post('/register')
     async register(@Body() registerDto: RegisterDto): Promise<{ token: string }> {
         return this.authService.register(registerDto)
+    }
+
+    @Post('/login')
+    async login(@Body() loginDto: LoginDto): Promise<{ token: string }> {
+        return this.authService.login(loginDto);
     }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User } from './schemas/user.schema';
 import * as mongoose from 'mongoose';
 import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -16,7 +17,12 @@ export class AuthService {
     }
 
     async register(registerDto: RegisterDto): Promise<{ token: string }> {
-        const { username, email, password } = registerDto;
+        const { username, email, password } = registerDto;        
+        
+        const isExist = await this.userModel.findOne({ email });
+        if(isExist) {
+            throw new ConflictException(`${email} is already exist !`);
+        }
 
         const salt = await bcrypt.genSalt(10);
         const hashedpassword = await bcrypt.hash(password, salt);
@@ -27,7 +33,27 @@ export class AuthService {
             password: hashedpassword
         });
 
-        const payload = { username: user.username, id: user.userId };
+        const payload = { username: user.username, id: user._id };
+        const token = this.jwtService.sign(payload);
+
+        return { token };
+    }
+
+    async login(loginDto: LoginDto): Promise<{ token: string }> {
+        const { email, password } = loginDto;
+
+        const user = await this.userModel.findOne({ email });
+
+        if(!user) {
+            throw new UnauthorizedException('Invalid email or password !');
+        }
+
+        const isPasswordMatched = await bcrypt.compare(password, user.password);
+        if(!isPasswordMatched) {
+            throw new UnauthorizedException('Invalid email or password !');
+        }
+
+        const payload = { username: user.username, id: user.id };
         const token = this.jwtService.sign(payload);
 
         return { token };
