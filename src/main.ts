344d4a6359b53b22de3987225e2809f73cae05af
yassine <yassinechmarekh88@gmail.com>
2025-04-11 13:07:20 +0100

    add cors configuration

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://task-pubsilon-frontend.vercel.app/'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
